04b7f6068eb5c1ac301b9598d2cac1df6bac5078
alviennn <alvien.rnp@gmail.com>
2023-05-31 20:18:55 +0700

    menambahkan class derivedClass denga pewarisan tetapi terjadi error pada pewarisan tersebut karena final pada baseclass

--- ParadigmaOOP_2/program noPewarisan/program noPewarisan.cpp
+++ ParadigmaOOP_2/program noPewarisan/program noPewarisan.cpp
@@ -7,3 +7,10 @@ public:
 		cout << "Halo saya Function dari base Class";
 	}
 };
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Halo saya Function dari derived Class";
+	}
+};
