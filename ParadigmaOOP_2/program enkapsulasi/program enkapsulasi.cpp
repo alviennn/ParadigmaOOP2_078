92cdb27aeb5f93e68a746500ea0559d9eb9c7dfd
alviennn <alvien.rnp@gmail.com>
2023-05-31 20:44:18 +0700

    menambahkan int main

--- ParadigmaOOP_2/program enkapsulasi/program enkapsulasi.cpp
+++ ParadigmaOOP_2/program enkapsulasi/program enkapsulasi.cpp
@@ -5,10 +5,26 @@ class remotelampu {
 private:
 	string saklarNo[10];
 public:
-	void setsaklarNo(int i) {
+	void setsaklarNo(int i, string value) {
 		saklarNo[i];
 	}
 	string getSaklarNo(int i) {
 		return saklarNo[i];
 	}
 };
+
+int main() {
+	remotelampu lampuRumah;
+
+	lampuRumah.setsaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setsaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setsaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setsaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	return 0;
+}
