c36310d4b5b5745d6ee2725136e28f6052286ea4
alviennn <alvien.rnp@gmail.com>
2023-05-31 20:31:26 +0700

    menambahkan akses modifier public pada class remote lampu dengan set dan get pada saklar lampu

--- ParadigmaOOP_2/program enkapsulasi/program enkapsulasi.cpp
+++ ParadigmaOOP_2/program enkapsulasi/program enkapsulasi.cpp
@@ -4,5 +4,11 @@ using namespace std;
 class remotelampu {
 private:
 	string saklarNo[10];
-
+public:
+	void setsaklarNo(int i) {
+		saklarNo[i];
+	}
+	string getSaklarNo(int i) {
+		return saklarNo[i];
+	}
 };
